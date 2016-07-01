231c48116df01ddf7d39833c0c4e0906bcc36d79
ileossa <swip.delta+gitt@gmail.com>
2016-07-01 22:26:45 +0200

    refacto, ajout de l'interface IProcessing, de de la class Processing. WIP: processing.resize()

---
+++ grayScale/IProcessing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grayScale
+{
+    interface IProcessing
+    {
+
+        Bitmap grayScaleProcessing();
+
+        Bitmap sepiaProcessing();
+
+        Bitmap negativProcessing();
+
+        Bitmap blueProcessing();
+
+        Bitmap redProcessing();
+
+        Bitmap greenProcessing();
+
+        /// <summary>
+        ///     Permet de changer la luminosité par défaut 100
+        ///         0 < 255  penche vers le blanc
+        ///         0 < -255 penche vers le noir
+        /// </summary>
+        /// <param name="picture"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        Bitmap brightnessProcessing(int range = 100);
+
+         
+        Bitmap contrastProcessing(double range = 50);
+
+        Bitmap resizeProcessing();
+        
+    }
+}
